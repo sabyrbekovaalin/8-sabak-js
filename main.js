b33953ecfde2f48e1e5d73c6f96b1e67b3b34344
saburbekovaalin <sabyrbekovaalina666@gmail.com>
2022-11-07 09:31:27 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// setTimeout(arg1, arg2)
+
+function test (){
+
+}
+
+setTimeout(() => {
+    console.log(test)
+},3000)
+Set
+
+incBtn.addEventListener('click', inc)
+// get github
+// setTimeOut() - setInterval()
+//Project - Tobo list
+
+
+
+
+
